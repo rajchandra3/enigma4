c96243ca985d56cf9e8f8dce3ef2e9bf33426a51
sahil3Vedi <st9800@gmail.com>
2018-02-10 18:15:38 +0530

    Added timer support and fixed index UI

--- public/assets/js/indexTemplate.js
+++ public/assets/js/indexTemplate.js
@@ -11,17 +11,10 @@ $(document).ready(function(){
   $('#logInHere').on('click touchstart', function(){
     $('#signUpForm').css({"display":"none"});
     $('#logInForm').css({"display":"block"});
-    $('#modal').css({"margin-top":"50vh"});
-    $('#modal').css({"transform":"translate(0,-50%)"});
   });
 
   $('#signUpHere').on('click touchstart', function(){
-      $('#logInForm').css({"display":"none"});
-      $('#signUpForm').css({"display":"block"});
-      $('#modal').css({"margin-top":"0"});
-      $('#modal').css({"transform":"translate(0,0)"});
-    });
+    $('#logInForm').css({"display":"none"});
+    $('#signUpForm').css({"display":"block"});
+  });
 });
-
-//margin-top: 50vh;
-//transform: translate(0,-50%);
---
+++ public/assets/js/timer.js
@@ -0,0 +1,36 @@
+var daySlot = document.getElementById('days');
+var hourSlot = document.getElementById('hours');
+var minuteSlot = document.getElementById('minutes');
+var secondSlot = document.getElementById('seconds');
+
+var dayLength=24*60*60*1000;
+var hourLength=60*60*1000;
+var minuteLength=60*1000;
+
+var time;
+var currentSecond;
+var currentHour;
+var currentMinute;
+
+var x=setInterval(function() {
+  time=new Date().getTime();
+  time=1519383000000-time;
+
+  currentSecond=Math.floor((time)/1000)%60;
+  currentDay=Math.floor(time/dayLength);
+  currentHour=Math.floor((time-currentDay*dayLength)/hourLength);
+  currentMinute=Math.floor((time-currentDay*dayLength-currentHour*hourLength)/minuteLength);
+
+  if(currentMinute===60){
+    currentHour++;
+    currentMinute=0;
+  }
+  if(currentHour===24){
+    currentDay++;
+    currentHour=0;
+  }
+  daySlot.innerHTML = currentDay;
+  hourSlot.innerHTML = currentHour;
+  minuteSlot.innerHTML = currentMinute;
+  secondSlot.innerHTML = currentSecond;
+},1000);
